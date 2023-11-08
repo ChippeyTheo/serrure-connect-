3311ffa84c5771cc34d704fbe64c64c97ab20e3a
ahliko <killian.guillemot@ynov.com>
2023-11-08 16:45:43 +0100

    Changes by kowai

--- code.py
+++ code.py
@@ -11,9 +11,20 @@ def iscode(entry: str) -> bool:
     return code == entry
 
 
+def enter_new_code(new: str) -> str:
+    print("Enter new code : ", end="")
+    while len(new) != len(code):
+        new += pave.getkey()
+        print(new, end="")
+    return new
+
+
 while True:
     userInput += pave.getkey()
     print(userInput)
+    if userInput[:-1] == 'C':
+        code = enter_new_code(code)
+        userInput = ""
     if len(userInput) == len(code):
         if iscode(userInput):
             print(":)")
