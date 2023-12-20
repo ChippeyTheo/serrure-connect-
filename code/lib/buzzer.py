9937c747f9715afa6d21ab7b4af4c1d865e6e212
ahliko <killian.guillemot@ynov.com>
2023-12-20 14:28:19 +0100

    Update buzzer.py

--- code/lib/buzzer.py
+++ code/lib/buzzer.py
@@ -3,11 +3,26 @@ import uasyncio as asyncio
 
 
 class Buzzer:
-    def __init__(self, pin):
-        self.pin = machine.Pin(pin, machine.Pin.OUT)
-        self.pin.off()
+    def __init__(self, pin, freq=1000):
+        self.pin = machine.PWM(machine.Pin(pin, machine.Pin.OUT))
+        self.freq = freq
+        self.__off()
+
+    def __off(self):
+        self.pin.duty_u16(0)
+
+    def __on(self):
+        self.pin.duty_u16(1000)
 
     async def beep(self, duration):
-        self.pin.on()
+        self.__on()
         await asyncio.sleep(duration)
-        self.pin.off()
+        self.__off()
+
+    @property
+    def freq(self):
+        return self.pin.freq()
+
+    @freq.setter
+    def freq(self, value):
+        self.pin.freq(value)
